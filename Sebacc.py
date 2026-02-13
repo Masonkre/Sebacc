94c31c50183cb8484ad11e0a541f6812627a106d
May <masonkrez@gmail.com>
2026-02-13 00:32:17 -0500

    Finished swapping action according to #3 and fixed indexing

--- Sebacc.py
+++ Sebacc.py
@@ -4,7 +4,7 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.index = None
+        #self.index = None
 
 class Table:
     def __init__(self):
@@ -34,12 +34,16 @@ class Hand:
 
     def add_card(self, card):
         self.cards.append(card)
-        card.index = len(self.cards)
+        #card.index = len(self.cards)
+
+    def reIndex(self):
+        for card in self.cards:
+            card.index = self.cards.index('')
 
     def displayCards(self):
         print("--- Cards in Hand ---")
-        for card in self.cards:
-            print(f'{card.index}.', card.suit, card.rank)
+        for i in range(len(self.cards)):
+            print(f'{i+1}.', self.cards[i].suit, self.cards[i].rank)
         print('\n')
 
     def gain(self, deck: object):
@@ -51,25 +55,20 @@ class Hand:
         return
     
     def discard(self, deck: object):
-        self.displayCards()
         discardIndex = int(input('Which card would you like to discard? (1,2,etc.): ')) 
         deck.discardPile.append(self.cards.pop(discardIndex - 1))
 
     def swap(self, deck: object):
-        self.displayCards()
         #swapIndex = int(input('Which card would you like to swap with? (1,2,etc.): '))
         self.discard(deck)
         self.add_card(deck.discardPile.pop(-2))
-        self.displayCards()
 
 
 def startGame():
     deck = Table()
     deck.shuffle()
     hand = Hand()
-
     deck.discardPile.append(deck.deal_card())
-
     '''
     /// Print statement for checking cards in deck
     for i in range(80):
@@ -83,21 +82,22 @@ def startGame():
 
 playing = True
 while playing:
-
     rounds = np.arange(1,5)
     phases = ['Turn', 'Bet', 'Spike']
     for round in rounds:
-        print(f'--- Round {round} --- \n')
-
+        print(f'\n -------- Round {round} -------- \n')
         if round == rounds[0]: # Start of a game
             deck, hand = startGame()
-
-        hand.displayCards()
+            hand.displayCards()
+        if round == rounds[-1]:
+            print('--- Showdown ---')
+            break
+        
 
         for phase in phases:
             print(f'--- {phase} Phase ---')
             if phase == 'Turn':
-                action = input('Gain, Swap, Stand, or Junk?')[:2].lower()
+                action = input('Gain, Swap, Stand, or Junk?: ')[:2].lower()
                 if action == 'ga':
                     hand.gain(deck)
 
@@ -109,12 +109,10 @@ while playing:
                     print('Standing')
                     pass
             
-                if action == 'ju':
+                if action == 'ju': # Find a way 
                     print('Junking')
-
-        if round == 4:
-            print('--- Showdown ---')
-            break
+                    break
+            hand.displayCards()
             
     playing = input('--- Want to play another round? (y/n) --- ').lower() in ('yes','y')
 
